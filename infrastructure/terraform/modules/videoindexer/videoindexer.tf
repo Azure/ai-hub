284b4cec647e71ff2dc775ddedaa0026eb85f168
Espen Brandt-Kjelsen <espen@microsoft.com>
2024-04-22 14:46:28 +0200

    vi update

--- infrastructure/terraform/modules/videoindexer/videoindexer.tf
+++ infrastructure/terraform/modules/videoindexer/videoindexer.tf
@@ -2,29 +2,24 @@ resource "azurerm_resource_group_template_deployment" "videoindexer" {
   name                = var.vi_service_name
   resource_group_name = var.resource_group_name
   deployment_mode     = "Incremental"
-  template_content = file("modules/videoindexer/templates/videoindexer.json",) 
-  parameters_content = jsonencode({
+  
+  parameters_content = jsonencode(
+    {
     "rgName" = {
       value = var.resource_group_name
-    },
+    }
     "location" = {
       value = var.location
-    },
+    }
     "prefix" = {
       value = var.prefix
-    },
+    }
     "videoSystemIdentity" = {
       value = var.videoSystemIdentity
-    },
+    }
     "videoMonCreation" = {
       value = var.videoMonCreation
+    }
   })
-  
-  {
-    rgName = var.resource_group_name
-    location = var.location
-    prefix = "ebk"
-    videoSystemIdentity = var.videoSystemIdentity
-    videoMonCreation = var.videoMonCreation
-  }
+  template_content = file("modules/videoindexer/templates/videoindexer.json",) 
 }
