e68ad5dbc6fa1a1b996e98d246265d66ec983192
uday31in <14359777+uday31in@users.noreply.github.com>
2024-04-22 18:33:55 +0100

    update

--- infrastructure/terraform/modules/videoindexer/data.tf
+++ infrastructure/terraform/modules/videoindexer/data.tf
@@ -1,7 +1,2 @@
 data "azurerm_client_config" "current" {
 }
-
-data "azurerm_user_assigned_identity" "user_assigned_identity" {
-  name                = local.cmk_uai.name
-  resource_group_name = local.cmk_uai.resource_group_name
-}
--- infrastructure/terraform/modules/videoindexer/vidoeindexer.tf
+++ infrastructure/terraform/modules/videoindexer/vidoeindexer.tf
@@ -18,6 +18,6 @@ resource "azapi_resource" "videoindexer" {
 
 resource "azurerm_role_assignment" "videoindexer" {
   scope                = var.storage_account_id
-  role_definition_name = "Storage Blob Data Contributor"
+  role_definition_name = "Storage Blob Data Owner"
   principal_id         = azapi_resource.videoindexer.identity[0].principal_id
 }
