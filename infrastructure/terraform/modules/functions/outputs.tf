d095d262989d8c586f80aee43305afe1b629ecd4
Marvin Buss <marvin.buss@gmail.com>
2024-04-28 18:55:33 +0200

    lint

--- infrastructure/terraform/modules/functions/outputs.tf
+++ infrastructure/terraform/modules/functions/outputs.tf
@@ -1,11 +1,11 @@
 output "function_assisstant_endpoint" {
   description = "Specifies the endpoint of the assisstant function."
-  sensitive = false
-  value = azurerm_linux_function_app.assistant_function.default_hostname
+  sensitive   = false
+  value       = azurerm_linux_function_app.assistant_function.default_hostname
 }
 
 output "function_shortclip_endpoint" {
   description = "Specifies the endpoint of the short clip function."
-  sensitive = false
-  value = azurerm_linux_function_app.shortclip_function.default_hostname
+  sensitive   = false
+  value       = azurerm_linux_function_app.shortclip_function.default_hostname
 }
