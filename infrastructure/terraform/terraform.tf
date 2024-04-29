c2fb62328530578ab6bf7d1cce5de2e12b73b111
Marvin Buss <marvin.buss@gmail.com>
2024-04-29 18:53:16 +0200

    Update provider versions

--- infrastructure/terraform/terraform.tf
+++ infrastructure/terraform/terraform.tf
@@ -4,11 +4,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~>3.100.0"
+      version = "3.101.0"
     }
     azapi = {
       source  = "azure/azapi"
-      version = "1.13.0"
+      version = "1.13.1"
     }
   }
 
