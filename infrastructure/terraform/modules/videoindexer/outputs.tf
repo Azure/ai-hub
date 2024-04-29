196d48bd5e4b97553bd63f75935ecc4a89b94d65
Marvin Buss <marvin.buss@gmail.com>
2024-04-29 13:47:50 +0200

    Add missing role assignment

--- infrastructure/terraform/modules/videoindexer/outputs.tf
+++ infrastructure/terraform/modules/videoindexer/outputs.tf
@@ -2,3 +2,8 @@ output "azapi_resource_videoindexer_id" {
   description = "Azure API Resource Video Indexer ID."
   value       = jsondecode(azapi_resource.videoindexer.output).properties.accountId
 }
+
+output "videoindexer_id" {
+  description = "Specifies the resource if of the Video Indexer."
+  value       = jsondecode(azapi_resource.videoindexer.output).properties.accountId
+}
