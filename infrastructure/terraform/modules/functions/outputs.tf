6dc8be755342bb98c3c8896b12dccc5dbe8a48d4
Marvin Buss <marvin.buss@gmail.com>
2024-04-29 14:05:27 +0200

    Fix wrong naming

--- infrastructure/terraform/modules/functions/outputs.tf
+++ infrastructure/terraform/modules/functions/outputs.tf
@@ -6,13 +6,7 @@ output "function_assisstant_endpoint" {
 
 output "function_assisstant_key" {
   description = "Specifies the key of the assisstant function."
-  sensitive   = false
-  value       = data.azurerm_function_app_host_keys.function_app_host_keys_shortclip.primary_key
-}
-
-output "function_assisstant_shortclip" {
-  description = "Specifies the key of the short clip function."
-  sensitive   = false
+  sensitive   = true
   value       = data.azurerm_function_app_host_keys.function_app_host_keys_shortclip.primary_key
 }
 
@@ -20,4 +14,10 @@ output "function_shortclip_endpoint" {
   description = "Specifies the endpoint of the short clip function."
   sensitive   = false
   value       = azurerm_linux_function_app.shortclip_function.default_hostname
+}
+
+output "function_shortclip_key" {
+  description = "Specifies the key of the short clip function."
+  sensitive   = true
+  value       = data.azurerm_function_app_host_keys.function_app_host_keys_shortclip.primary_key
 }
