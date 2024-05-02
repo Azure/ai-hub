45454a161ea6726536487bca1a8de449ec065937
Marvin Buss <marvin.buss@gmail.com>
2024-05-02 19:12:04 +0200

    Update role assignment for videoindexer

--- infrastructure/terraform/modules/videoindexer/roleassignments.tf
+++ infrastructure/terraform/modules/videoindexer/roleassignments.tf
@@ -1,5 +1,5 @@
 resource "azurerm_role_assignment" "videoindexer" {
   scope                = var.storage_account_id
-  role_definition_name = "Storage Blob Data Owner"
+  role_definition_name = "Storage Blob Data Contributor"
   principal_id         = azapi_resource.videoindexer.identity[0].principal_id
 }
