2b5eccbdf16dfe4f7319f800d3ad0d67779ce2a9
Marvin Buss <marvin.buss@gmail.com>
2024-05-03 11:38:38 +0200

    Update backend config

--- infrastructure/terraform/terraform.tf
+++ infrastructure/terraform/terraform.tf
@@ -18,6 +18,6 @@ terraform {
     storage_account_name = "<provided-via-config>"
     container_name       = "<provided-via-config>"
     key                  = "<provided-via-config>"
-    # use_azuread_auth     = true
+    use_azuread_auth     = true
   }
 }
