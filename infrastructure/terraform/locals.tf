b6634398c3d68cb4242434958db2b25de21a7cfa
uday31in <14359777+uday31in@users.noreply.github.com>
2024-05-09 11:44:11 +0100

    terraform fmt -recursive

--- infrastructure/terraform/locals.tf
+++ infrastructure/terraform/locals.tf
@@ -15,8 +15,8 @@ locals {
   ai_rg                 = "${local.prefix}-ai"
   videoindexer_name     = "${local.prefix}-vi"
   open_ai_name          = "${local.prefix}-aoai"
-  gpt_model_name        = "${lower(var.model_name)}"
-  gpt_model_version     = "${lower(var.model_version)}"
+  gpt_model_name        = lower(var.model_name)
+  gpt_model_version     = lower(var.model_version)
   search_service_name   = lower(replace("${local.prefix}-search", "/[^a-z0-9]/", ""))
   docintel_service_name = "${local.prefix}-docintel"
 
