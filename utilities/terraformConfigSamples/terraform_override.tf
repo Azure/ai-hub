6abb0eff9aa9321bcab1a2e3fd38c8f85580bb40
Marvin Buss <marvin.buss@gmail.com>
2024-05-14 12:41:28 +0200

    Update tf override

--- utilities/terraformConfigSamples/terraform_override.tf
+++ utilities/terraformConfigSamples/terraform_override.tf
@@ -1,4 +1,21 @@
 terraform {
+  required_version = ">=0.14"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.103.1"
+    }
+    azapi = {
+      source  = "Azure/azapi"
+      version = "1.13.1"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "2.5.1"
+    }
+  }
+
   backend "local" {
     path = "./terraform.tfstate"
   }
